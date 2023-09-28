fd1ee138f38970fc7a2a01478a3aa8ebce71f700
mrzeszotarski <m.rzeszotarski45@gmail.com>
2023-09-28 21:21:43 +0200

    first

---
+++ webpage/models.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+class Beer(models.Model):
+
+    author = models.CharField(max_length=25)
+    date = models.DateField(default=timezone.now)
+    brewery = models.CharField(max_length=25)
+    name = models.CharField(max_length=30)
+    type = models.CharField(max_length=25)
+    style = models.CharField(max_length=25)
+    isapproved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.isapproved = True
+        self.save()
+
+    def __str__(self):
+        return self.name
+
