a0b235ab511480f594aa6964079fdfa5706acaab
mrzeszotarski <m.rzeszotarski45@gmail.com>
2023-12-03 16:11:54 +0100

    minor fixes

--- webpage/forms.py
+++ webpage/forms.py
@@ -1,8 +1,12 @@
 from django import forms
-from .models import Beer
+from .models import Beer, Review
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit, Field
+from crispy_forms.layout import Layout, Submit, Field
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth. models import User
 
+
+# Form for creation of the new beer in the database
 class BeerForm(forms.ModelForm):
 
     class Meta:
@@ -27,3 +31,43 @@ class BeerForm(forms.ModelForm):
             Field('blg', css_class="form10"),
         )
         self.helper.add_input(Submit('submit', 'Save'))
+
+
+# Form for creation of the new review (rating and comment about beer existing in the database)
+class ReviewForm(forms.ModelForm):
+
+    class Meta:
+        model = Review
+        fields = ('score', 'comment')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-exampleForm'
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'submit_survey'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+        self.helper.layout = Layout(
+            Field('score', css_class="form10"),
+            Field('comment', css_class="form50"),
+        )
+        self.helper.add_input(Submit('submit', 'Save'))
+
+
+# Form for creating new accounts
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
--- webpage/urls.py
+++ webpage/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from . import views
 
+# urls for created views
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('beer/new', views.beer_new, name='beer_new'),
     path('beer_list', views.beer_list, name='beer_list'),
     path('beer/<int:pk>/', views.beer_detail, name='beer_detail'),
-    path('beer/<int:pk>/edit', views.beer_edit, name ='beer_edit'),
+    path('beer/<int:pk>/edit', views.beer_edit, name='beer_edit'),
     path('beer/<int:pk>/remove', views.beer_remove, name='beer_remove'),
     path('beer/<int:pk>/approve', views.beer_approve, name='beer_approve'),
     path('approve_list', views.approve_list, name='approve_list'),
+    path('register', views.register_request, name='register'),
 ]
