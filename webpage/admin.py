3b3f26fece5a828483b59fbd3ee97530c5655e24
mrzeszotarski <m.rzeszotarski45@gmail.com>
2023-12-04 15:03:32 +0100

    e-commerce added

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Beer, Review
-
+from .models import Beer, Review, MyBeer, CartItem, Order
 # Registered models
 admin.site.register(Beer)
 admin.site.register(Review)
+admin.site.register(MyBeer)
+admin.site.register(CartItem)
+admin.site.register(Order)
--- webpage/forms.py
+++ webpage/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Beer, Review
+from .models import Beer, Review, MyBeer, Order
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Field
 from django.contrib.auth.forms import UserCreationForm
@@ -72,3 +72,61 @@ class NewUserForm(UserCreationForm):
         if commit:
             user.save()
         return user
+
+class MyBeerForm(forms.ModelForm):
+
+    class Meta:
+        model = MyBeer
+        fields = ('name', 'description', 'style', 'alcohol_content', 'blg', 'picture', 'malts', 'hops', 'additives',
+                  'price')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-exampleForm'
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'submit_survey'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+        self.helper.layout = Layout(
+            Field('name', css_class="form50"),
+            Field('description', css_class="form50"),
+            Field('style', css_class="form50"),
+            Field('alcohol_content', css_class="form10"),
+            Field('blg', css_class="form10"),
+            Field('picture', css_class="form50"),
+            Field('malts', css_class="form50"),
+            Field('hops', css_class="form50"),
+            Field('additives', css_class="form50"),
+            Field('price', css_class="form10"),
+        )
+        self.helper.add_input(Submit('submit', 'Save'))
+
+class OrderForm(forms.ModelForm):
+
+    class Meta:
+        model = Order
+        fields = ('name', 'surname', 'country', 'city', 'street', 'phone')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_id = 'id-exampleForm'
+        self.helper.form_class = 'blueForms'
+        self.helper.form_method = 'post'
+        self.helper.form_action = 'submit_survey'
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-8'
+        self.helper.layout = Layout(
+            Field('name', css_class="form50"),
+            Field('surname', css_class="form50"),
+            Field('country', css_class="form50"),
+            Field('city', css_class="form10"),
+            Field('street', css_class="form10"),
+            Field('phone', css_class="form50"),
+
+        )
+        self.helper.add_input(Submit('submit', 'Save'))
